faf6c7ffebbe7761e4722f5ba4476a4a5efcca05
albertanhduy <albertanhduy@gmail.com>
2017-10-25 09:38:03 +0700

    SachController

--- QuanLyThuVienHVKTQS/controller/SachController.cs
+++ QuanLyThuVienHVKTQS/controller/SachController.cs
@@ -14,6 +14,57 @@ namespace QuanLyThuVienHVKTQS.controller
         {
             db = new QuanLiThuVienHVKTQS();
         }
-       
+        public List<sach> Detail()
+        {
+            return db.saches.ToList();
+        }
+        public int Add(sach entity)
+        {
+            try
+            {
+                db.saches.Add(entity);
+                db.SaveChanges();
+            }
+            catch (Exception)
+            {
+                return 0;
+                throw;
+            }
+            return entity.masach;
+        }
+        public bool Edit(sach entity)
+        {
+            try
+            {
+                var index = db.saches.Find(entity.masach);
+                index.tensach = entity.tensach;
+                index.tentacgia = entity.tentacgia; index.manxb = entity.manxb;
+                index.giatien = entity.giatien; index.soluong = entity.soluong;
+                index.namxb = entity.namxb; index.solanxb = entity.solanxb;
+                index.theloai = entity.theloai; index.loai = entity.loai;
+                db.SaveChanges();
+            }
+            catch (Exception)
+            {
+                return false;
+                throw;
+            }
+            return true;
+        }
+        public bool Del(int masach)
+        {
+            try
+            {
+                var entity = db.saches.Find(masach);
+                db.saches.Remove(entity);
+                db.SaveChanges();
+            }
+            catch (Exception)
+            {
+                return false;
+                throw;
+            }
+            return true;
+        }
     }
 }
