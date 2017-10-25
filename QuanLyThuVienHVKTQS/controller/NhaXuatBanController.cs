e34f8460f300dfb41149504a116d7842d9d59c90
Diagru25 <31183495+Diagru25@users.noreply.github.com>
2017-10-25 11:07:05 +0700

    add, edit, del

--- QuanLyThuVienHVKTQS/controller/NhaXuatBanController.cs
+++ QuanLyThuVienHVKTQS/controller/NhaXuatBanController.cs
@@ -18,6 +18,50 @@ namespace QuanLyThuVienHVKTQS.controller
         {
             return db.nhaxuatbans.ToList();
         }
-
+        public int Add(nhaxuatban entity)
+        {
+            try
+            {
+                db.nhaxuatbans.Add(entity);
+                db.SaveChanges();
+            }
+            catch (Exception)
+            {
+                return 0;
+                throw;
+            }
+            return entity.manxb;
+        }
+        public bool Edit(nhaxuatban entity)
+        {
+            try
+            {
+                var index = db.nhaxuatbans.Find(entity.manxb);
+                index.tennxb = entity.tennxb;
+                index.diachi = entity.diachi; index.sdt = entity.sdt;
+                db.SaveChanges();
+            }
+            catch (Exception)
+            {
+                return false;
+                throw;
+            }
+            return true;
+        }
+        public bool Delete(int manxb)
+        {
+            try
+            {
+                var entity = db.nhaxuatbans.Find(manxb);
+                db.nhaxuatbans.Remove(entity);
+                db.SaveChanges();
+            }
+            catch (Exception)
+            {
+                return false;
+                throw;
+            }
+            return true;
+        }
     }
 }
