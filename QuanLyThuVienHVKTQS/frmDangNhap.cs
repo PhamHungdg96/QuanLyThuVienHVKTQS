72a3ddac42b065067b368612df608cf6b60a09d2
huygiangit96 <32055253+huygiangit96@users.noreply.github.com>
2017-10-27 16:03:26 +0700

    3

--- QuanLyThuVienHVKTQS/frmDangNhap.cs
+++ QuanLyThuVienHVKTQS/frmDangNhap.cs
@@ -20,7 +20,41 @@ namespace QuanLyThuVienHVKTQS
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            if (txtTenDangNhap.Text.Length > 0)
+            {
+                if (txtMatKhau.Text.Length > 0)
+                {
+                    NhanVienController nvc = new NhanVienController();
+                    int id = nvc.Login(txtTenDangNhap.Text, txtMatKhau.Text);
+                    if (id == -1)
+                    {
+                        lblLoi.Text = "Tên đăng nhập không tồn tại";
+                    }
+                    else if (id == 0)
+                    {
+                        lblLoi.Text = "Mật khẩu không hợp lệ";
+                    }
+                    else if (id == 1)
+                    {
+                        ConstantCommon.LOAI_TAI_KHOAN = 1;
+                        ConstantCommon.TEN_DANG_NHAP = txtTenDangNhap.Text;
+
+                        this.Hide();
+                        frmMain frm = new frmMain();
+                        frm.ShowDialog();
+                        this.Close();
+                    }
+                    else
+                    {
+                        ConstantCommon.LOAI_TAI_KHOAN = 2;
+                        ConstantCommon.TEN_DANG_NHAP = txtTenDangNhap.Text;
+                        this.Hide();
+                        frmMain frm = new frmMain();
+                        frm.ShowDialog();
+                        //Close();
+                    }
+                }
+            }
         }
     }
 }
