76cb5c1a658dffbe5430d5d8861a652abd4bcda9
phamhungdg96 <phamhungdg96@gmail.com>
2017-10-31 20:17:26 +0700

    sua controller va them common

--- QuanLyThuVienHVKTQS/controller/ConstantCommon.cs
+++ QuanLyThuVienHVKTQS/controller/ConstantCommon.cs
@@ -8,6 +8,8 @@ namespace QuanLyThuVienHVKTQS.controller
 {
     public class ConstantCommon
     {
-        
+        public static int LOAI_TAI_KHOAN = 0;
+        public static string TEN_DANG_NHAP = "";
+        public static string HO_TEN_NV = "";
     }
 }
--- QuanLyThuVienHVKTQS/controller/MuonTraSachController.cs
+++ QuanLyThuVienHVKTQS/controller/MuonTraSachController.cs
@@ -14,6 +14,75 @@ namespace QuanLyThuVienHVKTQS.controller
         {
             db = new QuanLiThuVienHVKTQS();
         }
-        
+        public List<muonsach> DSMuon(int sothe)
+        {
+            var result = db.muonsaches.Where(m => m.sothe == sothe && m.ngaytra == null).ToList();
+            return result;
+        }
+        public List<muonsach> DSLSMuon()
+        {
+            var result = db.muonsaches.Where(m => m.ngaytra == null).ToList();
+            return result;
+        }
+        public List<muonsach> DSLSTra()
+        {
+            var result = db.muonsaches.Where(m => m.ngaytra != null).ToList();
+            return result;
+        }
+        public List<muonsach> DSLS()
+        {
+            var result = db.muonsaches.ToList();
+            return result;
+        }
+        public int MuonSach(muonsach entity)
+        {
+            try
+            {
+                var index = db.muonsaches.Find(entity.id);
+                var sl = db.muonsaches.Where(m => m.masach == entity.masach && m.ngaytra == null).Count();
+                if (sl < db.saches.Find(entity.masach).soluong)
+                {
+                    if (index != null)
+                    {
+                        index.masach = entity.masach;
+                        index.tennv = entity.tennv;
+                        index.sothe = entity.sothe;
+                        index.ngaymuon = entity.ngaymuon;
+                        index.hantra = entity.hantra;
+                    }
+                    else
+                    {
+                        db.muonsaches.Add(entity);
+                    }
+                    db.SaveChanges();
+                }
+                else
+                {
+                    return 0;
+                }
+
+            }
+            catch (Exception)
+            {
+                return -1;
+                throw;
+            }
+            return 1;
+        }
+        public bool TraSach(long id)
+        {
+            try
+            {
+                var entity = db.muonsaches.Find(id);
+                entity.ngaytra = DateTime.Now;
+                db.SaveChanges();
+            }
+            catch (Exception)
+            {
+                return false;
+                throw;
+            }
+            return true;
+        }
     }
 }
